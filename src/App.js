bf8fd952a0ba77bee3a50df85576003b0368d80d
kushtriimhyseni <kushtrim@starlabspro.com>
2022-04-18 00:20:17 +0200

    Random Jokes API project

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import RandomJokesApi from "./components/RandomJokesApi";
+function App() {
+  return (
+    <>
+      <RandomJokesApi />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/RandomJokesApi.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
+const RandomJokesApi = () => {
+  const [userData, setUserData] = useState({});
+  const [text, setText] = useState("Click the button to get a joke");
+
+  const fetchData = async () => {
+    const response = await axios.get("https://api.chucknorris.io/jokes/random");
+    setUserData(response.data);
+  };
+  const clickHandler = () => {
+    fetchData();
+    setText("");
+  };
+  return (
+    <div>
+      <img src="https://i.ibb.co/5F5F532/chuck.png" />
+      <div id="display-joke" className="card">
+        <span className="random-text">{text}</span>
+        <span>{userData.value}</span>
+      </div>
+      <button onClick={clickHandler} className="glow-on-hover">
+        Get a Joke!
+      </button>
+    </div>
+  );
+};
+
+export default RandomJokesApi;
